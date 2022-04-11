068c0154738dd4d33a30fa978ff336f124e19579
jjking <2017929959@qq.com>
2022-04-11 16:35:38 +0800

    Update main.cpp

--- untitled193/main.cpp
+++ untitled193/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+using namespace std;
+int dp[100100];
 int main() {
-    std::cout << "Hello, github!" << std::endl;
+    int a,b,c;
+    cin>>a>>b>>c;
+    dp[a] = dp[b] = 1;
+    for(int i = min(a,b);i <= c;i ++) {
+        if(dp[i])
+            dp[i + a] = 1,dp[i + b] = 1;
+    }
+    dp[0] = 1;
+    for(int i = c;i >= 0;i --) {
+    
+        if(dp[i]) {
+            cout<<i;
+            return 0;
+        }
+    }
     return 0;
 }
